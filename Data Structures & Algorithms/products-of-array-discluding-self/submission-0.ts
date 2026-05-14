2f1515df19f1f1d4aca2ade3ae392309b5119d81
Nur Eko Purwanto <nureko2105@gmail.com>
2026-05-14 16:56:51 +0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const res : number[] = Array(nums.length).fill(1)
+
+        let prefix = 1
+        for(let i = 0; i < nums.length; i++) {
+            res[i] = prefix
+            prefix *= nums[i]
+        }
+
+        let suffix = 1
+        for (let i = nums.length - 1; i >= 0; i--) {
+            res[i] *= suffix
+            suffix *= nums[i]
+        }
+
+        return res
+    }
+}
